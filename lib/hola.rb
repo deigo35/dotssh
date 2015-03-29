997e4bf3598cf1f591b16cbdb1b8a941a948fa11
Sango Deigo <sgdeigo@gmail.com>
2015-03-29 09:23:53 -0400

    adding some more code

--- lib/hola.rb
+++ lib/hola.rb
@@ -1,5 +1,21 @@
 class Hola
-  def self.hi
-    puts "Hello World!"
+  def self.hi(language = "english")
+    translator = Translator.new(language)
+    translator.hi
+  end
+end
+
+class Hola::Translator
+  def initialize(language)
+    @language = language
+  end
+  
+  def hi
+    case @language
+    when "spanish"
+      "hola mundo"
+    else
+      "hello world"
+    end
   end
 end
